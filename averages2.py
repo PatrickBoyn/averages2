19ea105a379ea7ad2c0aa4b12f7520548f134365
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-12 16:16:43 -0400

    Added a database connection. Also close the connection after because I won't be doing anything with it for awhile.

--- averages2.py
+++ averages2.py
@@ -5,14 +5,15 @@ import psycopg2
 AVERAGES = input("Please enter a number, or (h)elp for help: \n")
 
 try:
-    CON = psycopg2.connect("dbname='' user='postgres' host='localhost'")
-    CUR = CON.cur()
+    CON = psycopg2.connect("dbname='averages2' user='postgres' host='localhost'")
+    CUR = CON.cursor()
 except:
     print("No database connected at this time. ")
+CUR.close()
 
 def main():
     print("This is main, am I working? The number you entered in was: " + AVERAGES)
 
 
 if __name__ == "main":
-    main()
+      main()
