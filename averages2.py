c1ef6fe02454e69ff3f99431c5a1c67c0da2d590
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-12 16:19:31 -0400

    Moved everything to main, and fixed my problem with main not working.

--- averages2.py
+++ averages2.py
@@ -1,19 +1,18 @@
 """This will be athe second version of my averages program. """
 import psycopg2
 
-
-AVERAGES = input("Please enter a number, or (h)elp for help: \n")
-
-try:
-    CON = psycopg2.connect("dbname='averages2' user='postgres' host='localhost'")
-    CUR = CON.cursor()
-except:
-    print("No database connected at this time. ")
-CUR.close()
-
 def main():
-    print("This is main, am I working? The number you entered in was: " + AVERAGES)
+    averages = input("Please enter a number, or (h)elp for help: \n")
+
+    try:
+        con = psycopg2.connect("dbname='averages2' user='postgres' host='localhost'")
+        cur = con.cursor()
+    except:
+        print("No database connected at this time. ")
+    cur.close()
+
+    print("This is main, am I working? The number you entered in was: " + averages)
 
 
-if __name__ == "main":
+if __name__ == "__main__":
       main()
