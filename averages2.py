81ec757228eed359ba5b4f76a0e702eeb9f5d769
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-12 16:10:30 -0400

    Added main and also changed the opening statement to be a bit more obvious that you could press either h or help.

--- averages2.py
+++ averages2.py
@@ -2,6 +2,17 @@
 import psycopg2
 
 
-AVERAGES = input("Please enter a number, or help for help: \n")
+AVERAGES = input("Please enter a number, or (h)elp for help: \n")
 
-print("You entered in " + AVERAGES)
+try:
+    CON = psycopg2.connect("dbname='' user='postgres' host='localhost'")
+    CUR = CON.cur()
+except:
+    print("No database connected at this time. ")
+
+def main():
+    print("This is main, am I working? The number you entered in was: " + AVERAGES)
+
+
+if __name__ == "main":
+    main()
