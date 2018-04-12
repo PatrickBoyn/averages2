30f56be3c873803b41a5f9b90a2e937922133adc
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-12 18:08:56 -0400

    Moved everything into an Averages2 class. The purpose of this is to practice working with classes.

--- averages2.py
+++ averages2.py
@@ -1,18 +1,19 @@
 """This will be athe second version of my averages program. """
 import psycopg2
 
-def main():
-    averages = input("Please enter a number, or (h)elp for help: \n")
+class Averages2():
+    def main():
+        averages = input("Please enter a number, or (h)elp for help: \n")
 
-    try:
-        con = psycopg2.connect("dbname='averages2' user='postgres' host='localhost'")
-        cur = con.cursor()
-    except:
-        print("No database connected at this time. ")
-    cur.close()
+        try:
+            con = psycopg2.connect("dbname='averages2' user='postgres' host='localhost'")
+            cur = con.cursor()
+        except:
+            print("No database connected at this time. ")
+        cur.close()
 
-    print("This is main, am I working? The number you entered in was: " + averages)
+        print("This is main, am I working? The number you entered in was: " + averages)
 
 
-if __name__ == "__main__":
-      main()
+    if __name__ == "__main__":
+        main()
