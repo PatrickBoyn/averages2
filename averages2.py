ce19eb4c5c454dc0da42d615d64f3329bb5f852d
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-13 12:13:32 -0400

    Added self variable and tried to get it to work with the if name main statement.

--- averages2.py
+++ averages2.py
@@ -2,9 +2,7 @@
 import psycopg2
 
 class Averages2():
-    def main():
-        averages = input("Please enter a number, or (h)elp for help: \n")
-
+    def connection(self):
         try:
             con = psycopg2.connect("dbname='averages2' user='postgres' host='localhost'")
             cur = con.cursor()
@@ -12,8 +10,13 @@ class Averages2():
             print("No database connected at this time. ")
         cur.close()
 
+ 
+    def main(self):
+        averages = input("Please enter a number, or (h)elp for help: \n")
+        self.connection()
+
         print("This is main, am I working? The number you entered in was: " + averages)
 
 
-    if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    Averages2.main()
