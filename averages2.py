8a5ec304603f351000c86049d4b740a49a2f9577
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-24 01:04:58 -0400

    Trying to figure out why I can't call the class.

--- averages2.py
+++ averages2.py
@@ -1,9 +1,11 @@
 """This will be athe second version of my averages program. """
+
+
 class Averages:
-    def __init__(self, num):
-        self.num = num
-    
-    def connection():
-        try:
-            
-        except:
+    def __init__(self):
+        pass
+
+    def connection(self):
+        print("This is a test.")
+
+Averages.connection()
