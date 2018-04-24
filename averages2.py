e68ba0db5d600b682555bc6fb76eca31d05210af
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-24 00:38:13 -0400

    Writing the bare bones.

--- averages2.py
+++ averages2.py
@@ -1,2 +1,9 @@
 """This will be athe second version of my averages program. """
-import psycopg2
+class Averages:
+    def __init__(self, num):
+        self.num = num
+    
+    def connection():
+        try:
+            
+        except:
