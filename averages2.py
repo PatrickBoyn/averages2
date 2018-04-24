174b1a124af54a572d96449dfab4a7133a9e3180
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-24 00:33:59 -0400

    I am rewriting the file.

--- averages2.py
+++ averages2.py
@@ -1,22 +1,2 @@
 """This will be athe second version of my averages program. """
 import psycopg2
-
-class Averages2():
-    def connection(self):
-        try:
-            con = psycopg2.connect("dbname='averages2' user='postgres' host='localhost'")
-            cur = con.cursor()
-        except:
-            print("No database connected at this time. ")
-        cur.close()
-
- 
-    def main(self):
-        averages = input("Please enter a number, or (h)elp for help: \n")
-        self.connection()
-
-        print("This is main, am I working? The number you entered in was: " + averages)
-
-
-if __name__ == "__main__":
-    Averages2.main()
