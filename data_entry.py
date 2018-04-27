12700e8d73736cbaaadd8917098a3609c2bf0454
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-26 23:48:05 -0400

    Added the basic connection to the database.

--- data_entry.py
+++ data_entry.py
@@ -1,2 +1,13 @@
 """Puts a number into the database. """
-from averages2 import Averages2
+import psycopg2 as pg
+ 
+class Connection:
+    def __init__(self, db):
+        self.db = db
+
+    def db_setup(self):
+        conn = pg.connect(dbname=self.db, user='postgres', host='localhost')
+        cur = conn.cursor()
+
+class AddNumber(Connection):
+    pass
